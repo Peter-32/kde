c0e2cb252835b345c0941b4a0681338dfb5aca79
Peter-32 <peter@impactradius.com>
2022-05-22 00:38:26 -0700

    init

--- 1.py
+++ 1.py
@@ -10,21 +10,15 @@ housing = fetch_california_housing()
 m, n = housing.data.shape
 housing_data = pd.DataFrame(housing.data)
 housing_data.columns = ["_" + str(x) for x in housing_data.columns]
+
+
+# Sample code
 X = housing_data[["_0"]]
 print(X)
-
-
 x = np.linspace(X.min(), X.max(), 1000)
-
-
-
 kde = KernelDensity(kernel='gaussian', bandwidth=0.5).fit(X)
 y = kde.score_samples(x)
-sns.kdeplot(data=housing_data, x="_0")
+
+# Plot code
+sns.kdeplot(data=housing_data, x="_0", fill=True, alpha=.5, linewidth=0, cmap="Blues")
 plt.show()
-# plt.plot(x, y)
-# X.hist()
-# plt.show()
-#
-#
-# plt.show()
--- 2.py
+++ 2.py
@@ -10,21 +10,16 @@ housing = fetch_california_housing()
 m, n = housing.data.shape
 housing_data = pd.DataFrame(housing.data)
 housing_data.columns = ["_" + str(x) for x in housing_data.columns]
+print(housing_data)
+
+# Sample code
 X = housing_data[["_0"]]
 print(X)
-
-
 x = np.linspace(X.min(), X.max(), 1000)
-
-
-
 kde = KernelDensity(kernel='gaussian', bandwidth=0.5).fit(X)
 y = kde.score_samples(x)
-sns.kdeplot(data=housing_data, x="_0", y="_1")
+
+# Plot code
+# sns.kdeplot(data=housing_data, x="_0", y="_1", fill=True, thresh=0, levels=100, cmap="mako")
+sns.kdeplot(data=housing_data, x="_0", y="_1", fill=True, cmap="Blues")
 plt.show()
-# plt.plot(x, y)
-# X.hist()
-# plt.show()
-#
-#
-# plt.show()
